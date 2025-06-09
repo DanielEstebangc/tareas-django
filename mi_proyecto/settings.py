80baf3ea6ef321b0b41c7c85e263be9fadb6f8ce
Daniel Esteban <danielgalvis.3107@gmail.com>
2025-06-09 11:01:11 -0500

    cambios para subir con render

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nxsif9q4fwtaw^oqg-0h#-$)j1d#p1t&))tvf3x*nrg-ssl9ev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
